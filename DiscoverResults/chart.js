98a2bc9da179d20e7ccad1ba26bf1301779b86fa
Hristiyan <hriso156@gmail.com>
2023-03-21 15:25:06 +0000

    WIP

--- DiscoverResults/chart.js
+++ DiscoverResults/chart.js
@@ -1,57 +1,22 @@
-// Define a function to fetch data from the PHP file
-async function getData() {
-    const response = await fetch('get_data.php');
-    const data = await response.json();
-    return data;
-}
+// Get the canvas element from the HTML file
+var ctx = document.getElementById("myChart").getContext("2d");
 
-// Call the function to get the data and create the chart
-getData().then(data => {
-  const temperatures = data.map(row => row.temperature);
-  const measurement_times = data.map(row => row.measurement_time);
-
-  const ctx = document.getElementById('temperatureChart').getContext('2d');
-  const temperatureChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: measurement_times,
-      datasets: [{
-        label: 'Temperature',
-        data: temperatures,
-        borderColor: 'rgb(255, 99, 132)',
-        borderWidth: 1,
-        fill: false
-      }]
+// Define the data for your line graph
+var chartData = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: "rgb(75, 192, 192)",
+      tension: 0.1,
     },
-    options: {
-      scales: {
-        xAxes: [{
-          type: 'time',
-          time: {
-            displayFormats: {
-              'millisecond': 'MMM DD, YYYY h:mm:ss.SSS a',
-              'second': 'MMM DD, YYYY h:mm:ss a',
-              'minute': 'MMM DD, YYYY h:mm a',
-              'hour': 'MMM DD, YYYY hA',
-              'day': 'MMM DD, YYYY',
-              'week': 'MMM DD, YYYY',
-              'month': 'MMM YYYY',
-              'quarter': '[Q]Q - YYYY',
-              'year': 'YYYY',
-            }
-          },
-          scaleLabel: {
-            display: true,
-            labelString: 'Measurement Time'
-          }
-        }],
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'Temperature'
-          }
-        }]
-      }
-    }
-  });
-}); 
+  ],
+};
+
+// Create a new line chart and display it on the canvas element
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: chartData,
+});
